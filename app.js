a8fdbf8a26ccd383816a13506b55da7c86ab332d
Odia Adodo <subzbelow@gmail.com>
2023-10-13 18:01:45 +0100

    Update

--- app.js
+++ app.js
@@ -13,18 +13,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Set up the MongoDB connection //useFindAndModify: false <-- Deprecated.,
-const mongoURL = 'mongodb://localhost:27017/';
-// mongoose.connect(mongoURL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-// })
+// Set up the MongoDB connection NB://useFindAndModify: false & useCreateIndex: true <-- Deprecated.,
+const mongoURL = 'mongodb://localhost:27017//music';
 mongoose.connect(mongoURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true, // Add this line
-  useFindAndModify: false,
 })
 .then(() => {
  console.log('Connected to MongoDB');
@@ -51,3 +44,9 @@ else if (process.env.NODE_ENV === 'staging') {
 } else if (process.env.NODE_ENV === 'production') {
 } 
 //Mount on AWS : https://engineering.99x.io/deploying-net-core-application-to-aws-app-runner-with-cdk-d271744c8a6c
+// mongoose.connect(mongoURL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true, // Add this line
+//   useFindAndModify: false,
+// })
